756246e81dcecbb00502cb73f224b6f59efeeac5
Doug Wegscheid <dwegscheid@sbcglobal.net>
2018-09-27 15:27:18 -0400

    Use -y on ffmpeg.

--- smaller_make_movie.sh
+++ smaller_make_movie.sh
@@ -6,9 +6,9 @@
 
 #ffmpeg -pattern_type glob -i "resized/*.png" resized_movie.avi
 
-ffmpeg -pattern_type glob -i "resized/*.png" -c:v libxvid resized_movie_xvid.avi
+ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v libxvid resized_movie_xvid.avi
 
 #ffmpeg -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 1 resized_movie_xvid_qv1.avi
 
-ffmpeg -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 10 resized_movie_xvid_qv10.avi
+ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 10 resized_movie_xvid_qv10.avi
 
