264dda7f1c591457778687a019de7f52f33c5533
Doug Wegscheid <dwegscheid@sbcglobal.net>
2018-10-11 13:18:04 -0400

    Don't make bad quality movies anymore.

--- smaller_make_movie.sh
+++ smaller_make_movie.sh
@@ -6,7 +6,7 @@
 
 #ffmpeg -pattern_type glob -i "resized/*.png" resized_movie.avi
 
-ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v libxvid resized_movie_xvid.avi
+#ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v libxvid resized_movie_xvid.avi
 
 #ffmpeg -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 1 resized_movie_xvid_qv1.avi
 
