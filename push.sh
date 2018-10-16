9f9dd77b9a8067a815536760926696cc21442d8d
Doug Wegscheid <dwegscheid@sbcglobal.net>
2018-10-16 11:51:54 -0400

    Add script to push to synology

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#rsync -av pi@3620-pi-camera-1.local:/var/log/LOCAL\* /home/wegscd/3620-pi-camera-1/
+rsync -av --progress \
+	$HOME/3620-pi-camera-1/timelapse/01/ \
+	wegscd@synology1.local:/volume1/wegscd/timelapse_01
