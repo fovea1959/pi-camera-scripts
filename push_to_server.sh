70b00c4681ff7f9a63a472f0c3067865b88df706
Doug Wegscheid <dwegscheid@sbcglobal.net>
2018-10-30 19:40:43 -0400

    synology1 got reorganized, so change rsync.

--- push_to_server.sh
+++ push_to_server.sh
@@ -2,4 +2,4 @@
 #rsync -av pi@3620-pi-camera-1.local:/var/log/LOCAL\* /home/wegscd/3620-pi-camera-1/
 rsync -av --progress \
 	$HOME/3620-pi-camera-1/timelapse/01/ \
-	wegscd@synology1.local:/volume1/wegscd/timelapse_01
+	wegscd@synology1.local:/volume1/homes/wegscd/timelapse_01
