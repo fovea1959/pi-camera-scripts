b1756b8150ac9fe49277c1e7ab163efff8fbe914
Doug Wegscheid <dwegscheid@sbcglobal.net>
2018-11-04 11:30:55 -0500

    Add suse stuff.

--- smaller_make_movie.sh
+++ smaller_make_movie.sh
@@ -2,5 +2,8 @@
 
 #ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 10 resized_movie_xvid_qv10.avi
 
-ffmpeg -y -r 5 -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 10 resized_movie_xvid_qv10_slow.avi
+#ffmpeg -y -r 5 -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 10 resized_movie_xvid_qv10_slow.avi
 
+ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_suse.avi 
+
+ffmpeg -y -r 5 -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_slow_suse.avi 
