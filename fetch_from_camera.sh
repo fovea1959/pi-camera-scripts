9ebfe2e987200d877c733a18544feff3b2f4ac07
Doug Wegscheid <dwegscheid@sbcglobal.net>
2018-12-29 13:28:32 -0500

    Do selective xfer first. Try to speed up rsync.

--- fetch_from_camera.sh
+++ fetch_from_camera.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+
 rsync -av pi@3620-pi-camera-1.local:/var/log/LOCAL\* $HOME/3620-pi-camera-1/
-rsync -av --progress pi@3620-pi-camera-1.local:/home/pi/timelapse/01/ $HOME/3620-pi-camera-1/timelapse/01/
+rv=$?
+
+
+if [ $rv -eq 0 ] ; then
+  rsync -av --progress \
+	--include "*-??00*" \
+	--exclude "*" \
+	-e "ssh -T -c aes128-ctr -o Compression=no -x" \
+	pi@3620-pi-camera-1.local:/home/pi/timelapse/01/ \
+	$HOME/3620-pi-camera-1/timelapse/01/
+  rv=$?
+fi
+
+if [ $rv -eq 0 ] ; then
+  rsync -av --progress \
+	-e "ssh -T -c aes128-ctr -o Compression=no -x" \
+	pi@3620-pi-camera-1.local:/home/pi/timelapse/01/ \
+	$HOME/3620-pi-camera-1/timelapse/01/
+  rv=$?
+fi
+
+echo status $rv
+
+exit $rv
--- pull_from_server.sh
+++ pull_from_server.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 #rsync -av pi@3620-pi-camera-1.local:/var/log/LOCAL\* /home/wegscd/3620-pi-camera-1/
+
+
+# first matching include or exclude is the one that takes effect
 rsync -av --progress \
+	--include "*-??00*" \
+	--exclude "*" \
+	-e "ssh -T -c aes128-ctr -o Compression=no -x" \
 	wegscd@synology1.local:/volume1/homes/wegscd/timelapse_01/ \
 	$HOME/3620-pi-camera-1/timelapse/01/ 
+rv=$?
+
+if [ $rv -eq 0 ] ; then
+  rsync -av --progress \
+	-e "ssh -T -c aes128-ctr -o Compression=no -x" \
+	wegscd@synology1.local:/volume1/homes/wegscd/timelapse_01/ \
+	$HOME/3620-pi-camera-1/timelapse/01/ 
+  rv=$?
+fi
+
+echo status $rv
+
+exit $rv
 
