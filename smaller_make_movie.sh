cf1c53d8e086618247fcead2de55ebad7b32e254
Doug Wegscheid <dwegscheid@sbcglobal.net>
2019-01-25 10:33:59 -0500

    Tweak smaller_make_movie

--- smaller_make_movie.sh
+++ smaller_make_movie.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 10 resized_movie_xvid_qv10.avi
+# used to be -c:v libxvid 
 
-#ffmpeg -y -r 5 -pattern_type glob -i "resized/*.png" -c:v libxvid -q:v 10 resized_movie_xvid_qv10_slow.avi
+ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 1 resized_movie_xvid_qv1_suse.avi 
 
-ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_suse.avi 
+#ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_suse.avi 
 
-ffmpeg -y -r 5 -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_slow_suse.avi 
+#ffmpeg -y -r 5 -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_slow_suse.avi 
