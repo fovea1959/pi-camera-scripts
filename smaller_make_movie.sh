88f433972c0765690362f73a9db18ea7105a8d89
Doug Wegscheid <dwegscheid@sbcglobal.net>
2019-03-05 15:18:03 -0500

    Make different size movies.

--- smaller_make_movie.sh
+++ smaller_make_movie.sh
@@ -4,6 +4,10 @@
 
 ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 1 resized_movie_xvid_qv1_suse.avi 
 
-#ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_suse.avi 
+ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 3 resized_movie_xvid_qv3_suse.avi 
+
+ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 5 resized_movie_xvid_qv5_suse.avi 
+
+ffmpeg -y -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_suse.avi 
 
 #ffmpeg -y -r 5 -pattern_type glob -i "resized/*.png" -c:v mpeg4 -vtag xvid -q:v 10 resized_movie_xvid_qv10_slow_suse.avi 
